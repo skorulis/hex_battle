9cdacd2f9430703213b4e410d791df2bf09dcaa1
Alex Skorulis <skorulis@gmail.com>
2021-03-13 09:52:02 +1100

    Initial Commit

---
+++ Shared/HexBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexBattleApp.swift
+//  Shared
+//
+//  Created by Alexander Skorulis on 13/3/21.
+//
+
+import SwiftUI
+
+@main
+struct HexBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
